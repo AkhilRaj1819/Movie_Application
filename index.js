21382b7b75c6b4b33d880f19d7048bbecbefdcc8
AkhilRaj1819 <vysyaraju.akhilraju.s89@kalvium.community>
2025-01-25 16:24:19 +0530

    movieapplication

---
+++ index.js
@@ -0,0 +1,31 @@
+const movies = document.getElementById("Movies");
+
+function fetchMovies() {
+    fetch("http://www.omdbapi.com/?i=tt3896198&apikey=3114b5ee&s=avengers")
+        .then((data) => {
+            return data.json();
+        })
+        .then((data) => {
+                data.Search.forEach((element) => {
+                    const div = document.createElement("div");
+                    div.className = "Movie-card";
+                    const img = document.createElement("img");
+                    const h3 = document.createElement("h3");
+
+                    img.src = element.Poster;
+                    h3.innerText = element.Title;
+                    div.append(img, h3);
+                    movies.appendChild(div);
+                    
+            console.log(data);
+
+                });
+            
+            
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+}
+
+fetchMovies();
